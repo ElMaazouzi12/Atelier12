c0c4542e9e4352af314cc7bbcbff7699ca296a4b
Elmaazouzi12 <maazouziabdellah@gmail.com>
2022-10-24 12:20:04 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import AddTaskForm from "./components/AddTaskForm"
+import ToDo from './components/ToDo'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  function App() {
+    const [toDo, setToDo] = useState([]);
+
+    // Temp State
+    const [newTask, setNewTask] = useState('');
+    
+    // Add task 
+    ///////////////////////////
+    const addTask = () => {
+      if (newTask) {
+        let num = toDo.length + 1;
+        let newEntry = { id: num, title: newTask, status: false }
+        setToDo([...toDo, newEntry])
+        setNewTask('');
+      }
+    }
+
+    // Delete task 
+    ///////////////////////////
+    const deleteTask = (id) => {
+      let newTasks = toDo.filter(task => task.id !== id)
+      setToDo(newTasks);
+    }
+
+    return (
+      <div className="App">
+        <div className="container App">
+
+
+          <h2>To Do List App</h2>
+
+
+          <AddTaskForm
+            newTask={newTask}
+            setNewTask={setNewTask}
+            addTask={addTask}
+          />
+
+          {/* Display ToDos */}
+
+          {toDo && toDo.length ? '' : 'No Tasks...'}
+
+          <ToDo
+            toDo={toDo}
+            deleteTask={deleteTask}
+          />
+
+        </div>
+      </div>
+    );
+  }
 
 export default App;
